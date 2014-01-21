6a78c1c2489858f9a290335fb05325ae49d83f3c
Richard Wills <richardwills90@gmail.com>
2014-01-21 17:17:59 +0000

    Inserts peeper split after 3 (default value) list items, and wraps the remaining ones.

--- jquery.peeper.js
+++ jquery.peeper.js
@@ -2,17 +2,37 @@
 	$.fn.peeper = function(options) {
 
 		var defaults = {
-			peepsies: 3000,
-			peepington: 1000,
+			peeps: 'li',
+			peepsToShow: 3,
 		}
 		var options =  $.extend(defaults, options);
 		
-		
 		return this.each(function() {
-
-			var peeps = $(this);
-			console.log(peeps.html());
+			var peep = $(this);
+			var listLength =peep.find(options.peeps).length;
+			var blah = '<div class="peeper">';
 			
+			peep.find(options.peeps).each(function(index, element){
+				
+			/*	if(index == options.peepsToShow){
+					$(this).before('<div class="peeper">');
+				} else if (index == (listLength - 1)){
+					$(this).after('</div>');
+				}
+				console.log(index);*/
+			
+			
+				if(index >= options.peepsToShow){
+					blah = blah + $(this).wrap('<p/>').parent().html();
+					$(this).unwrap();
+					console.log(blah);
+					$(this).remove();
+				}
+			});
+					blah = blah + '</div>';
+					peep.append(blah);
+			//console.log(peep.html());
+			//console.log(options.child);
 			
 		});
 	};
