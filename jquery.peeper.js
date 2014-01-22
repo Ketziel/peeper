09cd5a9504d17e15f5555305a32a762382272714
Richard Wills <richardwills90@gmail.com>
2014-01-22 12:39:41 +0000

    Added functionality to apply CSS Transition Classes on display toggle

--- jquery.peeper.js
+++ jquery.peeper.js
@@ -6,49 +6,47 @@
 			peepsToShow: 3,
 			expandText: 'MORE',
 			viewMoreIdentifier: 'peeper-clicker',
+			anim: '',
+			animClose: 'closed'
 		}
 		var options =  $.extend(defaults, options);
 		
 		return this.each(function() {
 			var peep = $(this);
 			var listLength =peep.find(options.peeps).length;
-			var blah = '<div class="peeper">';
+			var contentToBeHidden = '<div class="peeper ' + ((options.anim != '') ? options.anim : '') + '">';
 			
 			peep.find(options.peeps).each(function(index, element){
-				
-			/*	if(index == options.peepsToShow){
-					$(this).before('<div class="peeper">');
-				} else if (index == (listLength - 1)){
-					$(this).after('</div>');
-				}
-				console.log(index);*/
-			
-			
+
 				if(index >= options.peepsToShow){
-					blah = blah + $(this).wrap('<p/>').parent().html();
+					contentToBeHidden = contentToBeHidden + $(this).wrap('<p/>').parent().html();
 					$(this).unwrap();
-					console.log(blah);
 					$(this).remove();
 				}
 			});
-					blah = blah + '</div>';
-					peep.append(blah);
-					peep.after('<a href="#" class="' + options.viewMoreIdentifier + '">'+options.expandText+'</a>');
-						
-					//alert($(peep).html());
-						
-					$(peep.next('.' + options.viewMoreIdentifier)).click(function(e){
-						var thePeeper = $(this).prev('ul').find('.peeper');
-						thePeeper.slideToggle();
-						e.preventDefault();
-					});
-								
-					
-			//console.log(peep.html());
-			//console.log(options.child);
-			
+			contentToBeHidden = contentToBeHidden + '</div>';
+			peep.append(contentToBeHidden);
+			peep.after('<a href="#" class="' + options.viewMoreIdentifier + '">'+options.expandText+'</a>');
+			togglePeeperVisibility(peep.find('.peeper'));	
+			$(peep.next('.' + options.viewMoreIdentifier)).click(function(e){
+				togglePeeperVisibility($(this).prev('ul').find('.peeper'));
+				e.preventDefault();
+			});
+
 		});
 		
+		
+		function togglePeeperVisibility(thePeeper){
+			if (options.anim == ''){
+				thePeeper.slideToggle();
+			} else {
+				if (thePeeper.hasClass(options.animClose)){
+					thePeeper.removeClass(options.animClose);
+				} else {
+					thePeeper.addClass(options.animClose);
+				}
+			}
+		};
 
 		
 		
